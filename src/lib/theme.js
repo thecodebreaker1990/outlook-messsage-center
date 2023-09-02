022c68df129d0abcdddcec62a48415a6c0fb87ca
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2023-09-02 15:08:13 +0530

    feat: add roboto font to the project

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,11 +1,26 @@
 import { createTheme } from '@mui/material/styles';
+import Roboto from '../fonts/Roboto/Roboto-Regular.ttf';
 
 const theme = createTheme({
   typography: {
-    fontSize: 14 // This sets the base font size to 14px
+    fontSize: 14, // This sets the base font size to 14px
     // You can also customize other typography properties here
+    fontFamily: 'Roboto, sans-serif'
   },
-  spacing: 10
+  spacing: 10,
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Roboto';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('Roboto'), local('Roboto-Regular'), url(${Roboto}) format('ttf');
+        }
+      `
+    }
+  }
 });
 
 export default theme;
