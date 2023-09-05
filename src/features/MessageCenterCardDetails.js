959d43afc367282382bf64c88ec33e9fb4a62f42
Smriti <smrtghoshal24@gmail.com>
2023-09-05 20:37:57 +0530

    chore: Implement design changes to the MessageCenterCardDetails.

--- src/features/MessageCenterCardDetails.js
+++ src/features/MessageCenterCardDetails.js
@@ -1,9 +1,11 @@
 import { Box, Button, Typography, Avatar } from '@mui/material';
-import { deepPurple } from '@mui/material/colors';
+import { green, deepPurple } from '@mui/material/colors';
 
 function MessageCenterCardDetails() {
   return (
-    <Box sx={{ backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '8px', p: 2 }}>
+    <Box
+      sx={{ backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '10px', p: 2, m: 2 }}
+    >
       <Typography variant="h4">Nuevo mantenimiento programado</Typography>
       <Box sx={{ display: 'flex', alignItems: 'center', my: 1 }}>
         <Avatar sx={{ bgcolor: deepPurple[500], width: 24, height: 24 }}>OS</Avatar>
@@ -17,26 +19,28 @@ function MessageCenterCardDetails() {
           Hoy at 1:33pm
         </Typography>
       </Box>
-      <Box sx={{ display: 'flex', pl: '10', border: '1px solid #ccc' }}>
+      <Box sx={{ display: 'flex', border: '1px solid #ccc', p: 4 }}>
         <img
-          src="/assets/person/ad.jpg"
+          src="/assets/person/person1.jpg"
           alt="description"
-          style={{ width: '100px', height: 'auto', marginRight: '16px' }}
+          style={{ width: '10', marginRight: '30px' }}
         />
-        <Box>
+        <Box sx={{ flex: 1 }}>
           <Typography variant="body1">Medellin, 23 de Junio 2021</Typography>
           <Typography variant="body1">Propiedad QWY874</Typography>
+
           <Typography variant="body2" sx={{ my: 2 }}>
             Reciban un cordial saludo, muchas gracias por contactamos. De acuerdo con la
-            conversacioN, tenemos el gusto de presentar este documento GRUPO ZLIPERS.
+            conversacioN, tenemos el gusto de presentar este documento{' '}
+            <span style={{ fontWeight: 'bold' }}>GRUPO ZLIPERS.</span>
           </Typography>
-        </Box>
 
-        <Box sx={{ display: 'flex', alignItems: 'center', mt: '10' }}>
-          <Button variant="contained" color="primary">
-            Firmar EEP
-          </Button>
-          <Typography variant="body2" sx={{ ml: 2 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+            <Button size="small" sx={{ bgcolor: green[500], color: '#fff' }}>
+              Firmar EEP
+            </Button>
+          </Box>
+          <Typography variant="body2">
             Cualquier inquietud comunicarse a support@zlipers.com
           </Typography>
         </Box>
