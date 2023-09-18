5e3f6253afa18cacf2ca82858936ab2b9849fd4d
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2023-09-19 00:23:54 +0530

    chore: modify input signature field

--- src/components/Input/InputSignatureField.js
+++ src/components/Input/InputSignatureField.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { InputLabel, Box } from '@mui/material';
 import BaseFormControl from '../BaseFormControl';
 import SignatureCanvas from 'react-signature-canvas';
 
 const InputSignatureField = ({ field, value, onChange, error, ...rest }) => {
+  const containerRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [containerWidth, setContainerWidth] = useState(100);
+
+  useEffect(() => {
+    if (containerRef.current && canvasRef.current) {
+      const { clientWidth } = containerRef.current;
+      console.log(clientWidth);
+      setContainerWidth(clientWidth);
+    }
+  }, [containerRef, canvasRef]);
+
   return (
     <BaseFormControl field={field} value={value} error={error} {...rest}>
       <InputLabel sx={{ mb: 1 }} htmlFor="signature-canvas">
         {field.label}
       </InputLabel>
       <Box
+        ref={containerRef}
         sx={{
           border: '1px solid #ccc',
           borderRadius: 4,
           maxHeight: 300,
+          width: '500px',
           overflow: 'hidden'
         }}
       >
         <SignatureCanvas
+          ref={canvasRef}
           penColor="green"
           canvasProps={{
-            width: 400, // This will get overwritten by CSS styles
+            width: containerWidth,
             height: 200,
             className: 'sigCanvas',
             id: 'signature-canvas'
